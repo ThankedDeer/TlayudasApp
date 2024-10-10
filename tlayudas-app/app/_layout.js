b34a5d5f4861af37370f7e6ebfb36f351fe0b96a
jhayco <carbajalmendez@gmail.com>
2024-10-09 21:08:35 -0600

    integracion del safe-area-context

--- tlayudas-app/app/_layout.js
+++ tlayudas-app/app/_layout.js
@@ -1,12 +1,15 @@
 import { View, StyleSheet } from "react-native";
 import { Slot } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function Layout() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <StatusBar style="light"/>
-      <Slot />
-    </View>
+    <SafeAreaProvider>
+      <View className="flex-1 items-center justify-center bg-slate-700">
+        <StatusBar style="light" />
+        <Slot />
+      </View>
+    </SafeAreaProvider>
   );
 }
--- tlayudas-app/app/index.js
+++ tlayudas-app/app/index.js
@@ -1,9 +1,13 @@
 import { Text, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
 import Login from "../components/login/Login";
 export default function index() {
+  const insets = useSafeAreaInsets();
+
   return (
-    <View>
-        <Login/>
+    <View style={{ paddingTop: insets.top, paddingBottom: insets.bottom }}>
+      <Login />
     </View>
-  )
+  );
 }

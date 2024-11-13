eafc851a017e72ccd136acc3a35bf6e13e625a60
jhayco <manolojose36@hotmail.com>
2024-11-12 23:19:47 -0600

    Funcionalidad agregada a los botones y mejora en la pantalla Roportes

--- components/inventario/Inventario.jsx
+++ components/inventario/Inventario.jsx
@@ -1,22 +1,37 @@
 import { ScrollView, View } from "react-native";
-import TopButtons from "./buttons/TopButtons";
 import ProductCard from "./ProductCard";
 import { useEffect, useState } from "react";
-import {data} from '../inventario/productosArray'
+import { data } from "../inventario/productosArray";
+import BtnBuscar from "./buttons/BtnBuscar";
+import BtnFiltrar from "./buttons/BtnFiltrar";
 
 export default function Inventario() {
   const [producto, setProducto] = useState([]);
+  const [busqueda, setBusqueda] = useState("");
+  const [categoriaSeleccionada, setCategoriaSeleccionada] = useState("");
 
-  useEffect(()=>{
+  useEffect(() => {
     setProducto(data);
-  }, [])
+  }, []);
+
+  // Filtra los productos basándose en el valor de búsqueda y la categoría
+  const productosFiltrados = producto
+    .filter((p) => p.nombre.toLowerCase().includes(busqueda.toLowerCase()))
+    .filter((p) =>
+      categoriaSeleccionada ? p.categoria === categoriaSeleccionada : true
+    );
 
   return (
     <ScrollView>
-      <TopButtons/>
+      <View className="flex-row justify-between px-6 py-4">
+        <BtnBuscar onBuscar={(texto) => setBusqueda(texto)} />
+        <BtnFiltrar
+          onFiltrar={(categoria) => setCategoriaSeleccionada(categoria)}
+        />
+      </View>
       <View className="flex-row flex-wrap justify-between px-10 w-screen">
-        {producto.map((p)=>(
-          <ProductCard producto={p} key={p.id}/>
+        {productosFiltrados.map((p) => (
+          <ProductCard producto={p} key={p.id} />
         ))}
       </View>
     </ScrollView>
--- components/inventario/buttons/BtnBuscar.jsx
+++ components/inventario/buttons/BtnBuscar.jsx
@@ -1,15 +1,23 @@
 import { useState } from "react";
-import { View, TextInput, Text } from "react-native";
+import { View, TextInput } from "react-native";
 import { SearchIcon } from "../Icons/BtnIcons";
 
-
-export default function BtnBuscar() {
+export default function BtnBuscar({ onBuscar }) {
   const [texto, setTexto] = useState("");
+
+  const handleChange = (text) => {
+    setTexto(text);
+    onBuscar(text);
+  };
 
   return (
     <View className="flex-row items-center space-x-3 p-2 bg-slate-200 rounded-md justify-center w-24">
       <SearchIcon />
-      <TextInput value={texto} onChangeText={setTexto} placeholder="buscar" />
+      <TextInput
+        value={texto}
+        onChangeText={handleChange}
+        placeholder="Buscar"
+      />
     </View>
   );
 }
